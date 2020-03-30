7061855e62cb651865fda072befb5cd6448b059b
eme hado <emehado@gmail.com>
2020-03-30 23:15:24 +0000

    Updates

---
+++ precache-manifest.1e188d19670f60018f7f63f857d87323.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50f1e7a3106dce037d5d68b6c6974103",
+    "url": "/Monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c24e65fea5a337c0b6bb",
+    "url": "/Monsters-rolodex/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "60556612e2e7350d5712",
+    "url": "/Monsters-rolodex/static/css/main.4fd02fa9.chunk.css"
+  },
+  {
+    "revision": "c24e65fea5a337c0b6bb",
+    "url": "/Monsters-rolodex/static/js/2.b5102e9c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters-rolodex/static/js/2.b5102e9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60556612e2e7350d5712",
+    "url": "/Monsters-rolodex/static/js/main.9e5688e0.chunk.js"
+  },
+  {
+    "revision": "f50133f2c4b44c266753",
+    "url": "/Monsters-rolodex/static/js/runtime-main.da3dab2d.js"
+  }
+]);
